4ea8e70ceb9853064c67bd3b63630140d624b549
Junlys3 <juliosdelima1@gmail.com>
2026-05-26 22:14:07 -0300

    Inserção no banco de dados

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
 <?php
-include("db.php");
+require("db.php");
 
 $sql = "SELECT * FROM posts_data"; // executa um sql
 //$result = $conn->query($sql);
 //echo "<pre>";
 //print_r($result);
+
+if (!empty($_POST['user_Data']) && !empty($_POST['text_Data'])) {
+  $user_Data = $_POST['user_Data'];
+  $text_Data = $_POST['text_Data'];
+
+  $store_Data = $conn->prepare("INSERT INTO posts_data (user_Data, text_Data) VALUES (?, ?)");
+  $store_Data->bind_param("ss", $user_Data, $text_Data);
+  $store_Data->execute();
+  echo "Dados armazenados com sucesso!";
+}
+
+
+
 
 
 ?>
@@ -24,6 +37,18 @@ $sql = "SELECT * FROM posts_data"; // executa um sql
     <div class="div-main">
       <h1 style="text-align: center;">Bem-vindo | Sem nome</h1>
       <hr>
+    </div>
+    <div class="div-form">
+      <form action="" method="post">
+        <label for="user_Data"></label>
+        <input type="text" name="user_Data" placeholder="Digite seu nome" id="user_Data">
+
+        <label for="text_Data"></label>
+        <textarea name="text_Data" id="text_Data"></textarea>
+
+        <input type="submit" value="Enviar">
+      </form>
+
     </div>
   </header>
   <main style="" class="container">
