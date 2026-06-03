1a67873be12d18f1dfe396df472311aa5f90b886
Junlys3 <juliosdelima1@gmail.com>
2026-06-02 22:08:32 -0300

    Alterações no banco

--- db.php
+++ db.php
@@ -1,10 +1,10 @@
 <?php
-$localhost = "localhost";
-$username = "root";
-$password = "";
-$dbname = "posts_data";
+$host = "sql201.infinityfree.com";
+$username = "if0_42083168";
+$password = "1478963Ju";
+$dbname = "if0_42083168_balacobase";
 
-$conn = new mysqli($localhost, $username, $password, $dbname);
+$conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
